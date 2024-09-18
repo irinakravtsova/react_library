184d1243245cfc98fd6281c35c3f71205103f7ca
Irina Kravtsova <kosmonavt980@gmail.com>
2024-09-18 19:52:45 +0300

    add card2.3

--- src/Components/FlexCard/FlexCard.jsx
+++ src/Components/FlexCard/FlexCard.jsx
@@ -1,8 +1,7 @@
 import classNames from 'classnames';
 import './FlexCard.css'
 import FotoCard from '../../assets/Restonans/1.jpg'
-import Rating from '../Rating/Rating'
-import Button from '../Button/button'
+
 
 function FlexCard(props) {
   
@@ -20,17 +19,47 @@ function FlexCard(props) {
               </div></li>
           <li className="flexcard__item">
           <div className="flexcard__card-2">
-            <div className="flexcard__card-2-prew">
-                Верхняя часть
+            <div className="flexcard__card-2-prew"> 
+                 
             </div>
             <div className="flexcard__card-2-body">
-              
-              </div>
-              <h2 className='fixcard__card-title'>Карточка 2</h2>
-              <p className="flexcard__desc">
+               <h2 className='fixcard__card-title'>Карточка 2</h2>
+              <p className="flexcard__desc-card-2">
                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit vero ipsum optio quam nihil autem.
               </p>
-              </div></li>
+            </div>              
+          </div>
+          </li>
+
+          <li className="flexcard__item">
+          <div className="flexcard__card-3">
+            <div className="flexcard__card-3-prew">
+               <img className='flexcard__card-3-img' src={FotoCard} alt="" /> 
+            </div>
+            <div className="flexcard__card-3-body">
+               <h2 className='fixcard__card-title'>Карточка 3</h2>
+              <p className="flexcard__desc-card-3">
+               Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit vero ipsum optio quam nihil autem.
+              </p>
+            </div>              
+          </div>
+           
+        </li>
+
+        <li className="flexcard__item">
+          <div className="flexcard__card-4">
+            <div className="flexcard__card-4-top">
+               <img className='flexcard__card-4-img' src={FotoCard} alt="" /> 
+            </div>
+            <div className="flexcard__card-4-battom">
+               <h2 className='fixcard__card-title'>Карточка 3</h2>
+              <p className="flexcard__desc-card-4">
+               Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit vero ipsum optio quam nihil autem.
+              </p>
+            </div>              
+          </div>
+           
+              </li>
           <li className="flexcard__item">Текст3</li>
           <li className="flexcard__item">Текст4</li>
         </ul>
